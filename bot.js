1591aca0052633eab6e61330658f576b4da72926
LuisLuis810 <Luisdiko23@icloud.com>
2025-06-22 11:29:50 +0000

    Play Dinosuar’s Power Is Strong

---
+++ bot.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+require('dotenv').config();
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+  console.log(`Logged in as ${client.user.tag}! Setting status to DND...`);
+  client.user.setStatus('dnd');
+});
+
+client.login(process.env.BOT_TOKEN);
